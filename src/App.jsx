d1f42f6640cfced70b2b5815db8a1b0ffbe31ef2
jihanv <jihanv@gmail.com>
2025-05-12 19:58:52 +0900

    Added keys to die using nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 import Die from './Die.jsx'
-
+import {nanoid} from "nanoid"
 function App() {
 
   function generateAllNewDice() {
     const dice = []
     for (let i = 0; i < 10; i++) {
-      dice.push(Math.floor(Math.random() * 6) + 1)
+      const die = {
+        value : Math.floor(Math.random() * 6) + 1,
+        isHeld: false,
+        id: nanoid()
+      }
+      dice.push(die)
     }
     return dice
   }
 
   const [dice, setDice] = useState(generateAllNewDice)
-  const eachDie = dice.map((number) => {
+  const eachDie = dice.map((die) => {
     return(
-      <Die value={number} />
+      <Die value={die.value} key={die.id}/>
     )
   })
   function rollDice(){
