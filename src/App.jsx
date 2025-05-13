e405ac4df7f512bd3d32204f98722d5067612a73
jihanv <jihanv@gmail.com>
2025-05-13 19:29:23 +0900

    Add sr-only notification for winning

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,7 @@ function App() {
     <>
       <main>
         {gameWon && <Confetti width={width} height={height} />}
+        <div aria-live="polite" className="sr-only">{gameWon && <p>Congratulations! You won!</p>}</div>
         <h1 className="title">Tenzies</h1>
         <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="dice-container">
