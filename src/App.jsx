14fabe92f7b0b21bf466a2aa0f0ac6fc6b42dbb3
jihanv <jihanv@gmail.com>
2025-05-13 18:04:56 +0900

    Add a check for winning conditions

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,35 @@ import './App.css'
 import Die from './Die.jsx'
 import { nanoid } from "nanoid"
 function App() {
+  const [dice, setDice] = useState(generateAllNewDice)
+
+  let gameWon = false;
+  if((dice.every(die => die.value === dice[0].value)) && (dice.every(die => die.isHeld === true))){
+    gameWon = true
+  }
+  else{
+    gameWon = false;
+  }
 
   function hold(id) {
-    setDice(prevDice => prevDice.map(die => {
-      return die.id === id ? { ...die, isHeld: !die.isHeld } : die
-    }))
-  }
+      setDice(prevDice => prevDice.map(die => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      }))
+    }
 
   function generateAllNewDice() {
-    const dice = []
-    for (let i = 0; i < 10; i++) {
-      const die = {
-        value: Math.floor(Math.random() * 6) + 1,
-        isHeld: false,
-        id: nanoid()
+      const dice = []
+      for (let i = 0; i < 10; i++) {
+        const die = {
+          value: Math.floor(Math.random() * 6) + 1,
+          isHeld: false,
+          id: nanoid()
+        }
+        dice.push(die)
       }
-      dice.push(die)
+      return dice
     }
-    return dice
-  }
 
-  const [dice, setDice] = useState(generateAllNewDice)
   const eachDie = dice.map((die) => {
     return (
       <Die
@@ -50,7 +58,7 @@ function App() {
         <div className="dice-container">
           {eachDie}
         </div>
-        <button className="roll-btn" onClick={rollDice}>Roll</button>
+        <button className="roll-btn" onClick={rollDice}>{gameWon? "New Game" : "Roll"}</button>
       </main>
     </>
   )
